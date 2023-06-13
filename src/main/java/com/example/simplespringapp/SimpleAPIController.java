dd5572fe4b4d5467722785b3a6dd8e361c910c31
Nikolai Schunk <nikolai@schunk.dev>
2023-06-13 11:37:16 +0200

    Implement Basic API Controller

--- src/main/java/com/example/simplespringapp/SimpleAPIController.java
+++ src/main/java/com/example/simplespringapp/SimpleAPIController.java
@@ -1,8 +1,29 @@
 package com.example.simplespringapp;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleAPIController {
+
+    @GetMapping("/api/example/")
+    public String getExample() {
+        // Implement the GET request logic here
+        return "GET request received";
+    }
+
+    @PostMapping("/api/example/")
+    public String postExample() {
+        // Implement the POST request logic here
+        return "POST request received";
+    }
+
+    @DeleteMapping("/api/example/")
+    public String deleteExample() {
+        // Implement the DELETE request logic here
+        return "DELETE request received";
+    }
+
 }
